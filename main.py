b46b980635358ecfeec2423e080c5d9ba8f10254
gloooow <stefanncula@gmail.com>
2022-12-12 02:07:01 +0100

    map images to information matrix

--- main.py
+++ main.py
@@ -10,48 +10,68 @@ cells = {
     1: {
         'first': '1.png',
         'second': (0, 0, 255),
+        'third': 1,
     },
     2: {
         'first': '2.png',
         'second': (0, 255, 0),
+        'third': 2,
     },
     3: {
         'first': '3.png',
         'second': (255, 0, 0),
+        'third': 3,
     },
     4: {
         'first': 'cell.png',
         'second': (0, 0, 0),
+        'third': 0,
     },
     5: {
         'first': 'empty_down.png',
-        'second': (255, 255, 255),
+        'second': (0, 255, 255),
+        'third': -1,
     },
     6: {
         'first': 'empty.png',
-        'second': (255, 255, 255),
+        'second': (0, 255, 255),
+        'third': -1,
     },
     7: {
         'first': 'empty_right.png',
-        'second': (255, 255, 255),
+        'second': (0, 255, 255),
+        'third': -1,
     },
     8: {
         'first': 'empty_corner.png',
-        'second': (255, 255, 255),
+        'second': (0, 255, 255),
+        'third': -1,
     },
     9: {
         'first': 'happy.png',
         'second': (0, 255, 255),
+        'third': '9'
     },
     10: {
         'first': 'sad.png',
         'second': (255, 0, 255),
+        'third': '-9'
     },
 }
 
+matrix = np.zeros((8, 8), dtype=object)
+
+def map_cells_to_matrix(point, value):
+    x, y = point
+    # x = x + 7
+    # y = y + 7
+    interpolated_x = np.interp(x, [10,122], [0,7])
+    interpolated_y = np.interp(y, [53,165], [0,7])
+    matrix[round(interpolated_y)][round(interpolated_x)] = {'first': value, 'second': (x, y), 'third': (round(interpolated_x), round(interpolated_y))}
+    # print("x: {}, y: {}, x_i: {}, y_i: {}, val: {}".format(x, y, interpolated_x, interpolated_y, value))
+
 def take_screen_shot():
-    # 800x600 windowed mode
-    screen = np.array(ImageGrab.grab(bbox=(0,0,150,250)))
+    screen = np.array(ImageGrab.grab(bbox=(6,49,146,231)))
     return screen
 
 def process_img(original_image):
@@ -67,7 +87,9 @@ def process_img(original_image):
             if(number['first'] == 'cell.png' or number['first'] == 'empty.png' or number['first'] == 'empty_corner.png' or number['first'] == 'empty_down.png' or number['first'] == 'empty_right.png'):
                 xOffset, yOffset = 1, 1
             cv2.rectangle(processed_img, (pt[0] + xOffset, pt[1] + yOffset), (pt[0] + w - xOffset - 1, pt[1] + h - yOffset - 1), number['second'])
-    
+            if(number['first'] != 'happy.png' and number['first'] != 'sad.png'):
+                map_cells_to_matrix((pt), number['third'])
+    print(position_matrix)
     return processed_img
 
 def main():
@@ -75,7 +97,7 @@ def main():
     while(True):
         screen = take_screen_shot()
         new_screen = process_img(screen)
-        # print('Loop took {} seconds'.format(time.time()-last_time))
+        print('Loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         cv2.imshow('window', new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -84,4 +106,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # pyautogui.moveTo(122 + 6 + 7, 165 + 49 + 7, duration=1)
+    # while True:
+    #     print(pyautogui.position())
+    #     time.sleep(1)
+
+    # while True:
+    #     pyautogui.moveTo(6, 49)
+    #     time.sleep(1)
+    #     pyautogui.moveTo(145, 231)
+    #     time.sleep(1)
 
